24a7202f5a5764031798ed095a0b3c5f71fae400
youneeo <steamdacut@gmail.com>
2024-01-12 00:06:18 +0900

    [feat] 장비 옵션명 조건 추가

--- app/src/main/java/com/android/maplemate/Adapter/SecondFragmentAdapter.kt
+++ app/src/main/java/com/android/maplemate/Adapter/SecondFragmentAdapter.kt
@@ -35,12 +35,120 @@ class SecondFragmentAdapter(val items: MutableList<Equipment.ItemEquipment?>) :
             binding.tvItemPartname.text = item?.itemEquipmentPart
             binding.tvItemName.text = item?.itemName
             binding.tvStarFoce.text = "${item?.starforce}성"
-            binding.tvOption1.text = item?.potentialOption1
-            binding.tvOption2.text = item?.potentialOption2
-            binding.tvOption3.text = item?.potentialOption3
-            binding.tvAddTvOption1.text = item?.additionalPotentialOption1
-            binding.tvAddTvOption2.text = item?.additionalPotentialOption2
-            binding.tvAddTvOption3.text = item?.additionalPotentialOption3
+            //옵션1 조건처리
+            when(item?.potentialOption1.toString()) {
+                "크리티컬 데미지 : +8%" -> binding.tvOption1.text = "크뎀 8%"
+                "보스 몬스터 공격 시 데미지 : +40%" -> binding.tvOption1.text = "보공40%"
+                "보스 몬스터 공격 시 데미지 : +35%" -> binding.tvOption1.text = "보공35%"
+                "보스 몬스터 공격 시 데미지 : +30%" -> binding.tvOption1.text = "보공30%"
+                "몬스터 방어율 무시 : +40%" -> binding.tvOption1.text = "방무40%"
+                "몬스터 방어율 무시 : +35%" -> binding.tvOption1.text = "방무35%"
+                "모든 스킬의 재사용 대기시간 : -2초(10초 이하는 10%감소, 5초 미만으로 감소 불가)" -> binding.tvOption1.text = "쿨감-2초"
+                "모든 스킬의 재사용 대기시간 : -1초(10초 이하는 5%감소, 5초 미만으로 감소 불가)" -> binding.tvOption1.text = "쿨감-1초"
+                "아이템 드롭률 : +20%" -> binding.tvOption1.text = "드랍 20%"
+                "메소 획득량 : +20%" -> binding.tvOption1.text ="메획 20%"
+                "캐릭터 기준 9레벨 당 STR : +2" -> binding.tvOption1.text ="렙당STR:+2"
+                "캐릭터 기준 9레벨 당 DEX : +2" -> binding.tvOption1.text ="렙당DEX:+2"
+                "캐릭터 기준 9레벨 당 INT : +2" -> binding.tvOption1.text ="렙당INT:+2"
+                "캐릭터 기준 9레벨 당 LUK : +2" -> binding.tvOption1.text ="렙당LUK:+2"
+                else -> binding.tvOption1.text = item?.potentialOption1
+            }
+
+            //옵션2 조건처리
+            when(item?.potentialOption2.toString()) {
+                "크리티컬 데미지 : +8%" -> binding.tvOption2.text = "크뎀 8%"
+                "보스 몬스터 공격 시 데미지 : +40%" -> binding.tvOption2.text = "보공40%"
+                "보스 몬스터 공격 시 데미지 : +35%" -> binding.tvOption2.text = "보공35%"
+                "보스 몬스터 공격 시 데미지 : +30%" -> binding.tvOption2.text = "보공30%"
+                "몬스터 방어율 무시 : +40%" -> binding.tvOption2.text = "방무40%"
+                "몬스터 방어율 무시 : +35%" -> binding.tvOption2.text = "방무35%"
+                "모든 스킬의 재사용 대기시간 : -2초(10초 이하는 10%감소, 5초 미만으로 감소 불가)" -> binding.tvOption2.text = "쿨감-2초"
+                "모든 스킬의 재사용 대기시간 : -1초(10초 이하는 5%감소, 5초 미만으로 감소 불가)" -> binding.tvOption2.text = "쿨감-1초"
+                "아이템 드롭률 : +20%" -> binding.tvOption2.text = "드랍 20%"
+                "메소 획득량 : +20%" -> binding.tvOption2.text ="메획 20%"
+                "캐릭터 기준 9레벨 당 STR : +2" -> binding.tvOption2.text ="렙당STR:+2"
+                "캐릭터 기준 9레벨 당 DEX : +2" -> binding.tvOption2.text ="렙당DEX:+2"
+                "캐릭터 기준 9레벨 당 INT : +2" -> binding.tvOption2.text ="렙당INT:+2"
+                "캐릭터 기준 9레벨 당 LUK : +2" -> binding.tvOption2.text ="렙당LUK:+2"
+                else -> binding.tvOption2.text = item?.potentialOption2
+            }
+
+            //옵션3 조건처리
+            when(item?.potentialOption3.toString()) {
+                "크리티컬 데미지 : +8%" -> binding.tvOption3.text = "크뎀 8%"
+                "보스 몬스터 공격 시 데미지 : +40%" -> binding.tvOption3.text = "보공40%"
+                "보스 몬스터 공격 시 데미지 : +35%" -> binding.tvOption3.text = "보공35%"
+                "보스 몬스터 공격 시 데미지 : +30%" -> binding.tvOption3.text = "보공30%"
+                "몬스터 방어율 무시 : +40%" -> binding.tvOption2.text = "방무40%"
+                "몬스터 방어율 무시 : +35%" -> binding.tvOption2.text = "방무35%"
+                "모든 스킬의 재사용 대기시간 : -2초(10초 이하는 10%감소, 5초 미만으로 감소 불가)" -> binding.tvOption3.text = "쿨감-2초"
+                "모든 스킬의 재사용 대기시간 : -1초(10초 이하는 5%감소, 5초 미만으로 감소 불가)" -> binding.tvOption3.text = "쿨감-1초"
+                "아이템 드롭률 : +20%" -> binding.tvOption3.text = "드랍 20%"
+                "메소 획득량 : +20%" -> binding.tvOption3.text ="메획 20%"
+                "캐릭터 기준 9레벨 당 STR : +2" -> binding.tvOption3.text ="렙당STR:+2"
+                "캐릭터 기준 9레벨 당 DEX : +2" -> binding.tvOption3.text ="렙당DEX:+2"
+                "캐릭터 기준 9레벨 당 INT : +2" -> binding.tvOption3.text ="렙당INT:+2"
+                "캐릭터 기준 9레벨 당 LUK : +2" -> binding.tvOption3.text ="렙당LUK:+2"
+                else -> binding.tvOption3.text = item?.potentialOption3
+            }
+            //에디1 조건처리
+            when(item?.additionalPotentialOption1.toString()) {
+                "크리티컬 데미지 : +3%" -> binding.tvAddTvOption1.text = "크뎀 3%"
+                "크리티컬 데미지 : +1%" -> binding.tvAddTvOption1.text = "크뎀 1%"
+                "보스 몬스터 공격 시 데미지 : +18%" -> binding.tvAddTvOption1.text = "보공18%"
+                "보스 몬스터 공격 시 데미지 : +12%" -> binding.tvAddTvOption1.text = "보공12%"
+                "모든 스킬의 재사용 대기시간 : -1초(10초 이하는 5%감소, 5초 미만으로 감소 불가)" -> binding.tvAddTvOption1.text = "쿨감-1초"
+                "아이템 드롭률 : +5%" -> binding.tvAddTvOption1.text = "드랍 5%"
+                "메소 획득량 : +5%" -> binding.tvAddTvOption1.text ="메획 5%"
+                "캐릭터 기준 9레벨 당 STR : +2" -> binding.tvAddTvOption1.text ="렙당STR:+2"
+                "캐릭터 기준 9레벨 당 DEX : +2" -> binding.tvAddTvOption1.text ="렙당DEX:+2"
+                "캐릭터 기준 9레벨 당 INT : +2" -> binding.tvAddTvOption1.text ="렙당INT:+2"
+                "캐릭터 기준 9레벨 당 LUK : +2" -> binding.tvAddTvOption1.text ="렙당LUK:+2"
+                "캐릭터 기준 9레벨 당 STR : +1" -> binding.tvAddTvOption1.text ="렙당STR:+1"
+                "캐릭터 기준 9레벨 당 DEX : +1" -> binding.tvAddTvOption1.text ="렙당DEX:+1"
+                "캐릭터 기준 9레벨 당 INT : +1" -> binding.tvAddTvOption1.text ="렙당INT:+1"
+                "캐릭터 기준 9레벨 당 LUK : +1" -> binding.tvAddTvOption1.text ="렙당LUK:+1"
+                else -> binding.tvAddTvOption1.text = item?.additionalPotentialOption1
+            }
+            //에디2 조건처리
+            when(item?.additionalPotentialOption2.toString()) {
+                "보스 몬스터 공격 시 데미지 : +18%" -> binding.tvAddTvOption2.text = "보공18%"
+                "보스 몬스터 공격 시 데미지 : +12%" -> binding.tvAddTvOption2.text = "보공12%"
+                "크리티컬 데미지 : +3%" -> binding.tvAddTvOption2.text = "크뎀 3%"
+                "크리티컬 데미지 : +1%" -> binding.tvAddTvOption2.text = "크뎀 1%"
+                "모든 스킬의 재사용 대기시간 : -1초(10초 이하는 5%감소, 5초 미만으로 감소 불가)" -> binding.tvAddTvOption2.text = "쿨감-1초"
+                "아이템 드롭률 : +5%" -> binding.tvAddTvOption2.text = "드랍 5%"
+                "메소 획득량 : +5%" -> binding.tvAddTvOption2.text ="메획 5%"
+                "캐릭터 기준 9레벨 당 STR : +2" -> binding.tvAddTvOption2.text ="렙당STR:+2"
+                "캐릭터 기준 9레벨 당 DEX : +2" -> binding.tvAddTvOption2.text ="렙당DEX:+2"
+                "캐릭터 기준 9레벨 당 INT : +2" -> binding.tvAddTvOption2.text ="렙당INT:+2"
+                "캐릭터 기준 9레벨 당 LUK : +2" -> binding.tvAddTvOption2.text ="렙당LUK:+2"
+                "캐릭터 기준 9레벨 당 STR : +1" -> binding.tvAddTvOption2.text ="렙당STR:+1"
+                "캐릭터 기준 9레벨 당 DEX : +1" -> binding.tvAddTvOption2.text ="렙당DEX:+1"
+                "캐릭터 기준 9레벨 당 INT : +1" -> binding.tvAddTvOption2.text ="렙당INT:+1"
+                "캐릭터 기준 9레벨 당 LUK : +1" -> binding.tvAddTvOption2.text ="렙당LUK:+1"
+                else -> binding.tvAddTvOption2.text = item?.additionalPotentialOption2
+            }
+            //에디3 조건처리
+            when(item?.additionalPotentialOption3.toString()) {
+                "보스 몬스터 공격 시 데미지 : +18%" -> binding.tvAddTvOption3.text = "보공18%"
+                "보스 몬스터 공격 시 데미지 : +12%" -> binding.tvAddTvOption3.text = "보공12%"
+                "크리티컬 데미지 : +3%" -> binding.tvAddTvOption3.text = "크뎀 3%"
+                "크리티컬 데미지 : +1%" -> binding.tvAddTvOption3.text = "크뎀 1%"
+                "모든 스킬의 재사용 대기시간 : -1초(10초 이하는 5%감소, 5초 미만으로 감소 불가)" -> binding.tvAddTvOption3.text = "쿨감-1초"
+                "아이템 드롭률 : +5%" -> binding.tvAddTvOption3.text = "드랍 5%"
+                "메소 획득량 : +5%" -> binding.tvAddTvOption3.text ="메획 5%"
+                "캐릭터 기준 9레벨 당 STR : +2" -> binding.tvAddTvOption3.text ="렙당STR:+2"
+                "캐릭터 기준 9레벨 당 DEX : +2" -> binding.tvAddTvOption3.text ="렙당DEX:+2"
+                "캐릭터 기준 9레벨 당 INT : +2" -> binding.tvAddTvOption3.text ="렙당INT:+2"
+                "캐릭터 기준 9레벨 당 LUK : +2" -> binding.tvAddTvOption3.text ="렙당LUK:+2"
+                "캐릭터 기준 9레벨 당 STR : +1" -> binding.tvAddTvOption3.text ="렙당STR:+1"
+                "캐릭터 기준 9레벨 당 DEX : +1" -> binding.tvAddTvOption3.text ="렙당DEX:+1"
+                "캐릭터 기준 9레벨 당 INT : +1" -> binding.tvAddTvOption3.text ="렙당INT:+1"
+                "캐릭터 기준 9레벨 당 LUK : +1" -> binding.tvAddTvOption3.text ="렙당LUK:+1"
+                else -> binding.tvAddTvOption3.text = item?.additionalPotentialOption3
+            }
+
 
 
 
