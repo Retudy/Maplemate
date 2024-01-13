11f160e7af8aa5f9d0fbd325c9b79c35beefcc94
youneeo <steamdacut@gmail.com>
2024-01-13 14:46:25 +0900

    [feat] 00시 ~ 04 시 사이에 데이터를 받아오지 못하는 문제 해결

--- app/src/main/java/com/android/maplemate/UI/SecondFragment.kt
+++ app/src/main/java/com/android/maplemate/UI/SecondFragment.kt
@@ -37,6 +37,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 
 class SecondFragment : Fragment() {
@@ -45,17 +47,19 @@ class SecondFragment : Fragment() {
     }
 
     private var _binding: FragmentSecondBinding? = null
+    private val Context.preferenceDataStore: DataStore<Preferences> by preferencesDataStore(name = "getOcid")
+    private val dataList = mutableListOf<Equipment.ItemEquipment?>()
+    private val adapter by lazy { SecondFragmentAdapter(dataList) }
     private val binding get() = _binding!!
     private lateinit var testApikey: String
     private lateinit var mapleNickName: String
-    private lateinit var getocid:String
+    private lateinit var getocid: String
+
     private val currentDate: LocalDate = LocalDate.now()
-    private val yesterday = currentDate.minusDays(2)
-    private val Context.preferenceDataStore: DataStore<Preferences> by preferencesDataStore(name = "getOcid")
-
-    private val dataList = mutableListOf<Equipment.ItemEquipment?>()
-    private val adapter by lazy { SecondFragmentAdapter(dataList) }
-
+    private var yesterday = currentDate.minusDays(1)
+    private val now: LocalDateTime = LocalDateTime.now()
+    private val startTime: LocalTime = LocalTime.of(0, 0)
+    private val endTime: LocalTime = LocalTime.of(4, 0)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,6 +68,15 @@ class SecondFragment : Fragment() {
         mapleNickName = ""
         getocid = ""
 
+        if (now.toLocalTime().isAfter(startTime) && now.toLocalTime().isBefore(endTime)) {
+            Log.d("TimeCheck", "현재 시간은 00시~04시 사이입니다(넥슨 홈페이지 업데이트 이전).")
+            yesterday = currentDate.minusDays(2)
+            Log.d("TimeCheck", "yesterday: ${yesterday}")
+        } else {
+            yesterday = currentDate.minusDays(1)
+            Log.d("TimeCheck", "현재 시간은 04시 이후입니다(업데이트 후).")
+            Log.d("TimeCheck", "yesterday: ${yesterday}")
+        }
     }
 
     override fun onCreateView(
@@ -86,14 +99,10 @@ class SecondFragment : Fragment() {
         binding.rvEqupipment.layoutManager = LinearLayoutManager(context) // (레이아웃 매니저 설정)
 
 
-
-
-
-
         binding.btnSearch.setOnClickListener {
-             mapleNickName = binding.searchView.text.toString()
+            mapleNickName = binding.searchView.text.toString()
 
-            if (mapleNickName.isNotBlank()){
+            if (mapleNickName.isNotBlank()) {
                 binding.apply {
                     binding.boxSearch.isVisible = false
                     binding.boxResult.isVisible = true
@@ -101,10 +110,9 @@ class SecondFragment : Fragment() {
                 lifecycleScope.launch {
                     apiRequest(mapleNickName)
                 }
-            }
-            else{
+            } else {
                 Toast.makeText(requireContext(), "입력값이없어 UI가 변경되지 않음", Toast.LENGTH_SHORT).show()
-                Log.d("nexon","입력값이없어 UI가 변경되지 않음")
+                Log.d("nexon", "입력값이없어 UI가 변경되지 않음")
             }
         }
         binding.ivBackButton.setOnClickListener {
@@ -117,17 +125,18 @@ class SecondFragment : Fragment() {
             lifecycleScope.launch {
                 delete()
             }
-            Log.d("nexon","dataStore 를 비웠습니다.")
+            Log.d("nexon", "dataStore 를 비웠습니다.")
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
 
     }
 
-    private fun apiRequest(mapleNickName:String) {
-    Log.d("nexon","$yesterday")
+    private fun apiRequest(mapleNickName: String) {
+        Log.d("nexon", "$yesterday")
         //1.Retrofit 객체 초기화
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://open.api.nexon.com")
@@ -149,38 +158,38 @@ class SecondFragment : Fragment() {
             val preferences = dataStore.data.first()
             val storedValue = preferences[key] ?: ""
 
-            if ( storedValue != "" ) {
-                Log.d("nexon","데이터를 로드하여 호출량 Count:-2 (장비/유니온)")
-                Log.d("nexon","입력받은 닉네임:${mapleNickName}")
+            if (storedValue != "") {
+                Log.d("nexon", "데이터를 로드하여 호출량 Count:-2 (장비/유니온)")
+                Log.d("nexon", "입력받은 닉네임:${mapleNickName}")
 
                 lifecycleScope.launch {
 
                     val loadedValue = load(mapleNickName).first()  //Flow에서 쿼리 = key 로 조회한 value값 추출
                     // 저장된값이 없을때 ( load() 함수를 보시면 string 값으로 value값을 저장해 "" 을 기본값으로 하였습니다.
-                    if (loadedValue != ""){
+                    if (loadedValue != "") {
                         var UnionCall = apiservicemaple.getUnion(
-                            testApikey,loadedValue,"${yesterday}"
+                            testApikey, loadedValue, "${yesterday}"
                         )
                         var characterCall = apiservicemaple.getCharacter(
-                            testApikey,loadedValue,"${yesterday}"
+                            testApikey, loadedValue, "${yesterday}"
                         )
                         var equipmentCall = apiservicemaple.getEquipment(
-                            testApikey,loadedValue,"${yesterday}"
+                            testApikey, loadedValue, "${yesterday}"
                         )
 
-                        characterCall.enqueue(object  : Callback<MapleData>{
+                        characterCall.enqueue(object : Callback<MapleData> {
                             override fun onResponse(
                                 call: Call<MapleData>,
                                 response: Response<MapleData>
                             ) {
                                 val data = response.body()
-                                Log.d("nexon","들어온 데이터:${response.body()}{}")
+                                Log.d("nexon", "들어온 데이터:${response.body()}{}")
 
                                 binding.ivCharacterImage.load(data?.characterImage)
                                 binding.tvCharacterLevel.text = "레벨:${data?.characterLevel}"
                                 binding.tvWorldName.text = "서버:${data?.worldName}"
                                 binding.tvCharacterName.text = "닉네임:${data?.characterName}"
-                                binding.tvCharacterGuildName.text = "길드명:${data?.characterGuildName}"
+                                binding.tvCharacterGuildName.text ="길드명:${data?.characterGuildName}"
                                 binding.tvCharacterExpRate.text = "경험치:${data?.characterExpRate}%"
                             }
 
@@ -188,7 +197,7 @@ class SecondFragment : Fragment() {
                                 call.cancel()
                             }
                         })
-                        UnionCall.enqueue(object  : Callback<MapleData>{
+                        UnionCall.enqueue(object : Callback<MapleData> {
                             override fun onResponse(
                                 call: Call<MapleData>,
                                 response: Response<MapleData>
@@ -207,7 +216,7 @@ class SecondFragment : Fragment() {
                         if (!dataList.isEmpty()) {
                             dataList.clear()
                         }
-                        equipmentCall.enqueue(object:Callback<Equipment>{
+                        equipmentCall.enqueue(object : Callback<Equipment> {
                             override fun onResponse(
                                 call: Call<Equipment>,
                                 response: Response<Equipment>
@@ -216,20 +225,21 @@ class SecondFragment : Fragment() {
 
 
                                 val itemEquipment = data?.itemEquipment
-                                Log.d("item","${data?.itemEquipment}")
+                                Log.d("item", "${data?.itemEquipment}")
 
 
 
 
                                 if (!itemEquipment.isNullOrEmpty()) {
-                                        val itemNames = itemEquipment.mapNotNull { it?.itemName } // 얘가 정상적으로 꺼내졌으니까
-                                        val itemsText = itemNames.joinToString("\n")
-                                        Log.d("Test","${itemsText}")
+                                    val itemNames =
+                                        itemEquipment.mapNotNull { it?.itemName } // 얘가 정상적으로 꺼내졌으니까
+                                    val itemsText = itemNames.joinToString("\n")
+                                    Log.d("Test", "${itemsText}")
 //                                        Log.d("item","item:${itemNames}")
-                                    itemEquipment?.let{ list ->
-                                       list.forEach {
-                                           dataList.add(it)
-                                       }
+                                    itemEquipment?.let { list ->
+                                        list.forEach {
+                                            dataList.add(it)
+                                        }
                                     }
 
                                     adapter.notifyDataSetChanged()
@@ -240,13 +250,13 @@ class SecondFragment : Fragment() {
                             }
 
                             override fun onFailure(call: Call<Equipment>, t: Throwable) {
-                               call.cancel()
+                                call.cancel()
                             }
                         })
                     }
-                }}
-            else{
-                Log.d("nexon","저장된 데이터가 없어 호출량 -3 (식벌자/기본정보/유니온")
+                }
+            } else {
+                Log.d("nexon", "저장된 데이터가 없어 호출량 -3 (식벌자/기본정보/유니온")
                 //4. 네트워크 통신
                 mapleCall.enqueue(object : Callback<MapleData> {
                     override fun onResponse(call: Call<MapleData>, response: Response<MapleData>) {
@@ -254,23 +264,23 @@ class SecondFragment : Fragment() {
                         val mapleInfo = response.body()
 
                         getocid = mapleInfo?.ocid.toString()
-                        lifecycleScope.launch{ save(mapleNickName) } // mapleCall을 부르게되면 즉시 키값을 저장함
-                            Log.d("testApikey","testApikey = ${testApikey} ")
+                        lifecycleScope.launch { save(mapleNickName) } // mapleCall을 부르게되면 즉시 키값을 저장함
+                        Log.d("testApikey", "testApikey = ${testApikey} ")
                         val characterCall = apiservicemaple.getCharacter(
                             testApikey,
                             "${getocid}",
                             "$yesterday"
                         )
                         var UnionCall = apiservicemaple.getUnion(
-                            testApikey,"${getocid}","${yesterday}"
+                            testApikey, "${getocid}", "${yesterday}"
                         )
                         var equipmentCall = apiservicemaple.getEquipment(
-                            testApikey,"${getocid}","${yesterday}"
+                            testApikey, "${getocid}", "${yesterday}"
                         )
                         //getocid 가 null 이 아니면 = getocid가 획득된경우 (이전에 로컬에서 조회한적이 없는경우)
-                        if(getocid != null){
+                        if (getocid != null) {
 
-                            characterCall.enqueue(object  : Callback<MapleData>{
+                            characterCall.enqueue(object : Callback<MapleData> {
                                 override fun onResponse(
                                     call: Call<MapleData>,
                                     response: Response<MapleData>
@@ -281,16 +291,16 @@ class SecondFragment : Fragment() {
                                     binding.tvCharacterLevel.text = "레벨:${data?.characterLevel}"
                                     binding.tvWorldName.text = "서버:${data?.worldName}"
                                     binding.tvCharacterName.text = "닉네임:${data?.characterName}"
-                                    binding.tvCharacterGuildName.text = "길드명:${data?.characterGuildName}"
-                                    binding.tvCharacterExpRate.text = "경험치:${data?.characterExpRate}%"
+                                    binding.tvCharacterGuildName.text ="길드명:${data?.characterGuildName}"
+                                    binding.tvCharacterExpRate.text ="경험치:${data?.characterExpRate}%"
                                 }
 
                                 override fun onFailure(call: Call<MapleData>, t: Throwable) {
-                                    Log.d("도착","${getocid}")
+                                    Log.d("도착", "${getocid}")
                                     call.cancel()
                                 }
                             })
-                            UnionCall.enqueue(object : Callback<MapleData>{
+                            UnionCall.enqueue(object : Callback<MapleData> {
                                 override fun onResponse(
                                     call: Call<MapleData>,
                                     response: Response<MapleData>
@@ -307,7 +317,7 @@ class SecondFragment : Fragment() {
                             if (!dataList.isEmpty()) {
                                 dataList.clear()
                             }
-                            equipmentCall.enqueue(object:Callback<Equipment>{
+                            equipmentCall.enqueue(object : Callback<Equipment> {
                                 override fun onResponse(
                                     call: Call<Equipment>,
                                     response: Response<Equipment>
@@ -315,17 +325,17 @@ class SecondFragment : Fragment() {
                                     val data = response.body()
 
                                     val itemEquipment = data?.itemEquipment
-                                    Log.d("item","${data?.itemEquipment}")
+                                    Log.d("item", "${data?.itemEquipment}")
 
 
 
 
                                     if (!itemEquipment.isNullOrEmpty()) {
-                                        val itemNames = itemEquipment.mapNotNull { it?.itemName } // 얘가 정상적으로 꺼내졌으니까
+                                        val itemNames =
+                                            itemEquipment.mapNotNull { it?.itemName } // 얘가 정상적으로 꺼내졌으니까
                                         val itemsText = itemNames.joinToString("\n")
-                                        Log.d("Test","${itemsText}")
-//                                        Log.d("item","item:${itemNames}")
-                                        itemEquipment?.let{ list ->
+                                        Log.d("Test", "${itemsText}")
+                                        itemEquipment?.let { list ->
                                             list.forEach {
                                                 dataList.add(it)
                                             }
@@ -352,6 +362,7 @@ class SecondFragment : Fragment() {
             }
         }
     }
+
     private suspend fun save(value: String): String {
         mapleNickName = binding.searchView.text.toString()
         val key = stringPreferencesKey(mapleNickName)
@@ -362,6 +373,7 @@ class SecondFragment : Fragment() {
         }
         return valueToSave
     }
+
     private suspend fun load(inputUserName: String): Flow<String> {
         val key = stringPreferencesKey(inputUserName)
         val dataStore = requireContext().preferenceDataStore
@@ -378,7 +390,8 @@ class SecondFragment : Fragment() {
                 loadedValue
             }
     }
-    private suspend fun delete(){
+
+    private suspend fun delete() {
         val dataStore = requireContext().preferenceDataStore
         dataStore.edit { preferences ->
             preferences.clear()
